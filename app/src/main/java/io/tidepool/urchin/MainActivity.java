b750be02e08946bc65578e0aba6343660cc13530
Brian King <bri@dufflite.com>
2015-09-02 10:38:14 -0700

    Basic note filter (needs some UI love)

--- app/src/main/java/io/tidepool/urchin/MainActivity.java
+++ app/src/main/java/io/tidepool/urchin/MainActivity.java
@@ -1,8 +1,8 @@
 package io.tidepool.urchin;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,18 +10,20 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -29,9 +31,15 @@ import org.w3c.dom.Text;
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
@@ -40,13 +48,16 @@ import io.realm.RealmResults;
 import io.tidepool.urchin.api.APIClient;
 import io.tidepool.urchin.data.Note;
 import io.tidepool.urchin.data.Profile;
-import io.tidepool.urchin.data.EmailAddress;
 import io.tidepool.urchin.data.SharedUserId;
 import io.tidepool.urchin.data.User;
 import io.tidepool.urchin.util.HashtagUtils;
 
 public class MainActivity extends AppCompatActivity implements RealmChangeListener, SwipeRefreshLayout.OnRefreshListener {
     private static final String LOG_TAG = "MainActivity";
+
+    // Special user IDs used for populating the filter menu
+    private static final String TAG_ALL_USERS = "AllUsers";
+    private static final String TAG_SIGNOUT = "SignOut";
 
     // What server we will connect to
     public static final String SERVER = APIClient.PRODUCTION;
@@ -56,13 +67,17 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
 
     private APIClient _apiClient;
 
+    // User to filter messages on, or null for all messages
+    private User _userFilter;
+
     // UI stuff
     private RecyclerView _recyclerView;
     private ImageButton _addButton;
     private RealmResults<Note> _notesResultSet;
     private SwipeRefreshLayout _swipeRefreshLayout;
-
+    private LinearLayout _dropDownLayout;
     private DateFormat _cardDateFormat = new SimpleDateFormat("EEEE MM/dd/yy h:mm a", Locale.getDefault());
+    private ListView _dropDownListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +89,9 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
 
         _swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
         _swipeRefreshLayout.setOnRefreshListener(this);
+
+        _dropDownLayout = (LinearLayout)findViewById(R.id.layout_drop_down);
+        _dropDownListView = (ListView)findViewById(R.id.listview_filter);
 
         _addButton = (ImageButton)findViewById(R.id.add_button);
         _addButton.setOnClickListener(new View.OnClickListener() {
@@ -118,18 +136,26 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
         setTitle(R.string.all_notes);
     }
 
-    protected void startQuery() {
+    protected void populateNotes() {
         Realm realm = Realm.getInstance(this);
 
         // Set up our query
-        _notesResultSet = realm.where(Note.class).findAllSorted("timestamp", false);
+        if ( _userFilter == null ) {
+            _notesResultSet = realm.where(Note.class).findAllSorted("timestamp", false);
+            setTitle(R.string.all_notes);
+        } else {
+            _notesResultSet = realm.where(Note.class).equalTo("userid", _userFilter.getUserid())
+                    .findAllSorted("timestamp", false);
+            setTitle(_userFilter.getProfile().getFullName());
+        }
+
         _recyclerView.setAdapter(new NotesAdapter());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        startQuery();
+        populateNotes();
         Realm realm = Realm.getInstance(this);
         realm.addChangeListener(this);
     }
@@ -156,16 +182,13 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_sign_out) {
-            _apiClient.signOut(new APIClient.SignOutListener() {
-                @Override
-                public void signedOut(int responseCode, Exception error) {
-                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivityForResult(loginIntent, REQ_LOGIN);
-                }
-            });
-            return true;
+        if ( id == R.id.action_filter_notes ) {
+            if ( _dropDownLayout.getVisibility() == View.GONE ) {
+                populateDropDownList();
+                _dropDownLayout.setVisibility(View.VISIBLE);
+            } else {
+                _dropDownLayout.setVisibility(View.GONE);
+            }
         }
 
         return super.onOptionsItemSelected(item);
@@ -235,6 +258,74 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
         } else {
             _swipeRefreshLayout.setRefreshing(false);
         }
+    }
+
+    private void populateDropDownList() {
+        Realm realm = Realm.getInstance(this);
+
+        // Unfortunately Realm does not support nested or joined queries, so we'll have to a bit
+        // of manual labor here.
+
+        // Get all of the profiles that do not have a null patient field. These are the only users
+        // who can post a message.
+        RealmResults<Profile> profiles = realm.where(Profile.class).isNotNull("patient").findAll();
+
+        // Create a set of user IDs from these profiles
+        Set<String> userIds = new HashSet<>();
+        for ( Profile profile : profiles ) {
+            userIds.add(profile.getUserId());
+        }
+
+        // Now get our list of users
+        List<User> users = new ArrayList<>();
+        for ( String userId : userIds ) {
+            User user = realm.where(User.class).equalTo("userid", userId).findFirst();
+            if ( user != null ) {
+                users.add(user);
+            } else {
+                Log.e(LOG_TAG, "Failed to find user with ID " + userId);
+            }
+        }
+
+        // Sort alphabetically by fullname, as that seems to be the only field guaranteed to be set
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User lhs, User rhs) {
+                return lhs.getProfile().getFullName().compareTo(rhs.getProfile().getFullName());
+            }
+        });
+
+        _dropDownListView.setAdapter(new UserFilterAdapter(this, R.layout.list_item_user, users));
+        _dropDownListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    // All users
+                    setUserFilter(null);
+                } else if (position == parent.getAdapter().getCount() - 1) {
+                    signOut();
+                } else {
+                    User user = (User) _dropDownListView.getAdapter().getItem(position);
+                    setUserFilter(user);
+                }
+                _dropDownLayout.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setUserFilter(User user) {
+        _userFilter = user;
+        populateNotes();
+    }
+
+    private void signOut() {
+        _apiClient.signOut(new APIClient.SignOutListener() {
+            @Override
+            public void signedOut(int responseCode, Exception error) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(loginIntent, REQ_LOGIN);
+            }
+        });
     }
 
     @Override
@@ -329,6 +420,65 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
             return _notesResultSet.size();
         }
 
+    }
+
+    public class UserFilterAdapter extends ArrayAdapter<User> {
+
+        public UserFilterAdapter(Context context, int resource, List<User> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public int getCount() {
+            // We have 2 additional rows to show, "All Users" and "Sign Out".
+            return super.getCount() + 2;
+        }
+
+        @Override
+        public User getItem(int index) {
+            if ( index == 0 ) {
+                User user = new User();
+                user.setUserid(TAG_ALL_USERS);
+                user.setFullName(getString(R.string.all_notes));
+                return user;
+            }
+
+            if ( index == getCount() - 1 ) {
+                User user = new User();
+                user.setUserid(TAG_SIGNOUT);
+                user.setFullName(getString(R.string.action_sign_out));
+                return user;
+            }
+
+            return super.getItem(index - 1);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if ( v == null ) {
+                v = getLayoutInflater().inflate(R.layout.list_item_user, parent, false);
+            }
+
+            User user = getItem(position);
+            TextView nameText = (TextView)v.findViewById(R.id.name_textview);
+            String name = user.getFullName();
+            if (TextUtils.isEmpty(name)){
+                name = user.getProfile().getFullName();
+            }
+            nameText.setText(name);
+
+            View indent = v.findViewById(R.id.indent);
+            if ( user.getUserid().equals(TAG_ALL_USERS) || user.getUserid().equals(TAG_SIGNOUT) ) {
+                indent.setVisibility(View.GONE);
+                nameText.setTypeface(null, Typeface.BOLD);
+            } else {
+                indent.setVisibility(View.VISIBLE);
+                nameText.setTypeface(null, Typeface.NORMAL);
+            }
+
+            return v;
+        }
     }
 
     @Override
